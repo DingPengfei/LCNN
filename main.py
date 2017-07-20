b50e09750fb09f6176d6cc897d5a25d89dc6b475
Administrator <ding.pf@qq.com>
2017-07-20 19:33:08 +0800

    first add

--- main.py
+++ main.py
@@ -1,50 +1,32 @@
 # -*- coding: utf-8 -*-
 import torch
+import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import scipy.io as sio
-from os import listdir
-from os.path import isfile, join
 import numpy as np
+import itertools
 
+# Hyper Parameters
+num_epochs = 10
+batch_size = 512
+learning_rate = 0.001
 
-# mypath = './data/'
-# files = [f for f in listdir(mypath) if f != 'jinshanwhole00.mat']
-# test = 'jinshanwhole00.mat'
-# size = len(files)
-#
-# for i in range(size):
-#     mat = sio.loadmat(mypath + files[i])['ecgBeatsInfo']
-#     mat_size = mat.shape[0]
-#     mats = [f.tolist() for f in mat[:, ]]
-#     normal = [f]
-#     # abnormal = [f for f in mat[:, ] if f[1, 5] == 1]
-#     pass
-
-
-
-
+# read data
 # @Author: Lv Cheng
 np.random.seed(10)
 Dimension = 1900
 single_sample = 560
-Samples = 44 * 560 + 29 # 24669
-
+Samples = 44 * 560 + 29  # 24669
 totalIter = 10000+1
-
 trainX = np.zeros([Samples - single_sample, 8, Dimension])
 trainY = np.zeros([Samples - single_sample, 1])
 testX = np.zeros([single_sample, 8, Dimension])
 testY = np.zeros([single_sample, 1])
 
-conv1_size = 21; conv1_feature = 6; max_pool1_size = 7
-conv2_size = 13; conv2_feature = 7; max_pool2_size = 6
-conv3_size =  9; conv3_feature = 5; max_pool3_size = 6
 
-fc_connect_size = 50
-
-def ReadDataSet():
+def read_data():
     global trainX, trainY, testX, testY
 
     X = np.zeros([Samples, 8, Dimension])
@@ -54,18 +36,10 @@ def ReadDataSet():
     for i in range(44):
         filename = 'jinshanwhole' + str(i).zfill(2) + '.mat'
 
-        # ecgBeatsInfo shape : 560 * 15205
-        # 15205 = 0~0 (order) +
-        # 1~1 (constant：1) +
-        # 2~2 (constant: 8) +
-        # 3~3 (constant: 1900) +
-        # 4~4 (classification 0:Normal 1:Abnormal) +
-        # 5~15204 = 8(II, III, V1, V2, V3, V4, V5, V6) * 1900
-
         mat = sio.loadmat(path + filename)['ecgBeatsInfo']
         # block_size = len(mat)	# 0~43:560, 44:29
 
-        st = i * 560;
+        st = i * 560
         en = st
         if (i < 44):
             en += 560
@@ -75,19 +49,7 @@ def ReadDataSet():
         X[st:en] = mat[:, 5:].reshape(-1, 8, Dimension)  # block_size * 8 * 1900
         Label[st:en] = mat[:, 4].reshape(-1, 1)  # block_size * 1
 
-    '''
-    # memory overflow
-    mean = X.mean(axis=2); std = X.std(axis=2)
-    X = (X - mean[:,:,np.newaxis])/(std[:,:,np.newaxis] + 1e-6)
-    '''
     print ("shape of X:", X.shape)
-    '''
-    mean = X.mean(axis=2); std = X.std(axis=2)
-    for i in range(np.shape(X)[0]):
-        for j in range(np.shape(X)[1]):
-            for k in range(np.shape(X)[2]):
-                X[i][j][k] = (X[i][j][k] - mean[i][j]) / (std[i][j] + 1e-6)
-    '''
 
     # split the dataset
     trainX = X[single_sample:, :, :]
@@ -99,13 +61,39 @@ def ReadDataSet():
     print ("Finish reading.")
 
 
+def next_batch(batch_size=128):
+    # should be integer of 0~42
+    index = np.random.randint(43)
+    # print ("train set is ", i)
+    batch_xs = np.zeros([batch_size, 8, Dimension-200, 1])
+    batch_ys = np.zeros([batch_size, 1])
+
+    for i in range(batch_size):
+        type = np.random.randint(2)
+        if type == 1:
+            # 1 ~ 160
+            x = np.random.randint(160)+1
+            start = np.random.randint(200)
+
+            batch_xs[i] = trainX[index*560+x, :, start:start+Dimension-200].reshape(-1, 8, Dimension-200, 1)
+            batch_ys[i] = trainY[index*560+x]
+        else:
+            # 161 ~ 560
+            x = np.random.randint(400)+161
+            start = np.random.randint(200)
+
+            batch_xs[i] = trainX[index*560+x, :, start:start+Dimension-200].reshape(-1, 8, Dimension-200, 1)
+            batch_ys[i] = trainY[index*560+x]
+
+    return batch_xs, batch_ys
 
 
-class Net(nn.Module):
+# LCNN Model
+class LCNN(nn.Module):
     # 定义Net的初始化函数，这个函数定义了该神经网络的基本结构
     def __init__(self):
 
-        super(Net, self).__init__()
+        super(LCNN, self).__init__()
 
         self.conv1_1 = nn.Conv1d(1, 6, 21)
         self.conv1_2 = nn.Conv1d(1, 6, 21)
@@ -134,41 +122,99 @@ class Net(nn.Module):
         self.conv3_7 = nn.Conv1d(7, 5, 9)
         self.conv3_8 = nn.Conv1d(7, 5, 9)
 
-        # combine 8 leads
+        self.fc1 = nn.Linear(8*5*5, 50)
 
-        self.fc1 = nn.Linear(16*5*5, 120)
-
-        self.fc2 = nn.Linear(120, 84)
-
-
+        self.fc2 = nn.Linear(50, 1)
 
     def forward(self, x):
 
-        x = F.max_pool1d(F.relu(self.conv1(x)), 7)
+        # xx1 = np.transpose(x[0])
+        # xx2 = np.transpose(x[0][0])
+        # xx3 = np.transpose(x[0][0][0])
+        # xx4 = torch.from_numpy(np.transpose(x[0]))
+        # xx4 = torch.from_numpy(np.transpose(x[0])).float()
+        x1 = Variable((torch.from_numpy(np.transpose(x[0]))).unsqueeze(0).float())
+        x2 = Variable((torch.from_numpy(np.transpose(x[1]))).unsqueeze(0).float())
+        x3 = Variable((torch.from_numpy(np.transpose(x[2]))).unsqueeze(0).float())
+        x4 = Variable((torch.from_numpy(np.transpose(x[3]))).unsqueeze(0).float())
+        x5 = Variable((torch.from_numpy(np.transpose(x[4]))).unsqueeze(0).float())
+        x6 = Variable((torch.from_numpy(np.transpose(x[5]))).unsqueeze(0).float())
+        x7 = Variable((torch.from_numpy(np.transpose(x[6]))).unsqueeze(0).float())
+        x8 = Variable((torch.from_numpy(np.transpose(x[7]))).unsqueeze(0).float())
 
-        x = F.max_pool1d(F.relu(self.conv1(x)), 6)
+        x1 = F.max_pool1d(F.relu(self.conv1_1(x1)), 7)
+        x2 = F.max_pool1d(F.relu(self.conv1_2(x2)), 7)
+        x3 = F.max_pool1d(F.relu(self.conv1_3(x3)), 7)
+        x4 = F.max_pool1d(F.relu(self.conv1_4(x4)), 7)
+        x5 = F.max_pool1d(F.relu(self.conv1_5(x5)), 7)
+        x6 = F.max_pool1d(F.relu(self.conv1_6(x6)), 7)
+        x7 = F.max_pool1d(F.relu(self.conv1_7(x7)), 7)
+        x8 = F.max_pool1d(F.relu(self.conv1_8(x8)), 7)
 
-        x = F.max_pool1d(F.relu(self.conv1(x)), 6)
+        x1 = F.max_pool1d(F.relu(self.conv2_1(x1)), 6)
+        x2 = F.max_pool1d(F.relu(self.conv2_2(x2)), 6)
+        x3 = F.max_pool1d(F.relu(self.conv2_3(x3)), 6)
+        x4 = F.max_pool1d(F.relu(self.conv2_4(x4)), 6)
+        x5 = F.max_pool1d(F.relu(self.conv2_5(x5)), 6)
+        x6 = F.max_pool1d(F.relu(self.conv2_6(x6)), 6)
+        x7 = F.max_pool1d(F.relu(self.conv2_7(x7)), 6)
+        x8 = F.max_pool1d(F.relu(self.conv2_8(x8)), 6)
 
-        x = x.view(-1, self.num_flat_features(x))
+        x1 = F.max_pool1d(F.relu(self.conv3_1(x1)), 6)
+        x2 = F.max_pool1d(F.relu(self.conv3_2(x2)), 6)
+        x3 = F.max_pool1d(F.relu(self.conv3_3(x3)), 6)
+        x4 = F.max_pool1d(F.relu(self.conv3_4(x4)), 6)
+        x5 = F.max_pool1d(F.relu(self.conv3_5(x5)), 6)
+        x6 = F.max_pool1d(F.relu(self.conv3_6(x6)), 6)
+        x7 = F.max_pool1d(F.relu(self.conv3_7(x7)), 6)
+        x8 = F.max_pool1d(F.relu(self.conv3_8(x8)), 6)
+
+        x1 = x1.view(-1, self.num_flat_features(x1))
+        x2 = x2.view(-1, self.num_flat_features(x2))
+        x3 = x3.view(-1, self.num_flat_features(x3))
+        x4 = x4.view(-1, self.num_flat_features(x4))
+        x5 = x5.view(-1, self.num_flat_features(x5))
+        x6 = x6.view(-1, self.num_flat_features(x6))
+        x7 = x7.view(-1, self.num_flat_features(x7))
+        x8 = x8.view(-1, self.num_flat_features(x8))
+
+        # combine 8 leads
+        x = torch.cat([x1, x2, x3, x4, x5, x6, x7, x8], dim=1)
 
         x = F.relu(self.fc1(x))
-
-        x = F.relu(self.fc2(x))
-
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
 
-    # 使用num_flat_features函数计算张量x的总特征量（把每个数字都看出是一个特征，即特征总量），比如x是4*2*2的张量，那么它的特征总量就是16。
     def num_flat_features(self, x):
-        # 这里为什么要使用[1:],是因为pytorch只接受批输入，也就是说一次性输入好几张图片，那么输入数据张量的维度自然上升到了4维。【1:】让我们把注意力放在后3维上面
-        size = x.size()[1:]
+        size = x.size()[1:]  # all dimensions except the batch dimension
         num_features = 1
         for s in size:
             num_features *= s
         return num_features
 
-ReadDataSet()
-net = Net()
-net
+lcnn = LCNN()
+lcnn.float()
+read_data()
 
+# Loss and Optimizer
+losses = []
+criterion = nn.BCELoss
+optimizer = optim.Adam(lcnn.parameters(), lr=learning_rate)
+
+# Train the Model
+for epoch in range(num_epochs):
+    batch_xs, batch_ys = next_batch(batch_size)
+    total_loss = torch.Tensor([0])
+    for data, target in itertools.izip(batch_xs, batch_ys):
+        # Forward + Backward + Optimize
+        lcnn.zero_grad()
+        # optimizer.zero_grad() ???
+        outputs = lcnn(data)
+        loss = criterion(outputs, target)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.data
+
+    losses.append(total_loss)
+
+# Test the Model
